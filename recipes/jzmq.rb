ed7a7f06b77a15d3896c8158256051130c9f831f
Matthew Hooker <mwhooker@gmail.com>
2013-01-16 03:35:01 -0800

    specify zmq prefix

--- recipes/jzmq.rb
+++ recipes/jzmq.rb
@@ -11,7 +11,7 @@ git node[:jzmq][:srcdir] do
   action :sync
 end
 execute "make_jzmq" do
-  command "JAVA_HOME=#{node['java']['java_home']} ./autogen.sh && ./configure && make && make install"
+  command "JAVA_HOME=#{node['java']['java_home']} ./autogen.sh && ./configure --with-zeromq=#{node[:zeromq][:install_dir]} && make && make install"
   cwd node[:jzmq][:srcdir]
   action :run
 end
