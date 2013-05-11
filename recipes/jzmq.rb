09861a7c6a7a9d8d72a480269ad8afe46c9f07d3
Homer Strong <homer.strong@gmail.com>
2013-05-10 17:54:06 -0700

    add workaround for installing jzmq on precise

--- recipes/jzmq.rb
+++ recipes/jzmq.rb
@@ -11,7 +11,8 @@ git node[:jzmq][:srcdir] do
   action :sync
 end
 execute "make_jzmq" do
-  command "JAVA_HOME=#{node['java']['java_home']} ./autogen.sh && ./configure --with-zeromq=#{node[:zeromq][:install_dir]} && make && make install"
+  # see https://github.com/zeromq/jzmq/issues/114
+  command "JAVA_HOME=#{node['java']['java_home']} sed -i 's/classdist_noinst.stamp/classnoinst.stamp/g' src/Makefile.am  && ./autogen.sh && ./configure --with-zeromq=#{node[:zeromq][:install_dir]} && make && make install"
   cwd node[:jzmq][:srcdir]
   action :run
 end
