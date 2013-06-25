6b96606f55496e374ce0ad6d169d2665f9ccbb28
Homer Strong <homer.strong@gmail.com>
2013-06-24 17:10:47 -0700

    clean jzmq src directory before attempting install

--- recipes/jzmq.rb
+++ recipes/jzmq.rb
@@ -1,15 +1,23 @@
 package "pkg-config"
 package "libtool"
 
+# clear any existing jzmq src dir, can cause permissions issues if an existing dir exists already
+directory node[:jzmq][:srcdir] do
+  rescursive true
+  action :delete
+end
+
 directory node[:jzmq][:srcdir] do
   mode 0755
   action :create
 end
+
 git node[:jzmq][:srcdir] do
   repository node[:jzmq][:repo]
   revision 'master'
   action :sync
 end
+
 execute "make_jzmq" do
   # see https://github.com/zeromq/jzmq/issues/114
   command "JAVA_HOME=#{node['java']['java_home']} sed -i 's/classdist_noinst.stamp/classnoinst.stamp/g' src/Makefile.am  && ./autogen.sh && ./configure --with-zeromq=#{node[:zeromq][:install_dir]} && make && make install"
