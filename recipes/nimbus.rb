ecdd08a53076b2a6ca71ae09db304a5bb0512082
Homer Strong <homer.strong@gmail.com>
2013-10-03 14:12:33 -0700

    clarify `install_dir`

--- recipes/nimbus.rb
+++ recipes/nimbus.rb
@@ -1,3 +1,6 @@
+storm_name = "storm-#{node[:storm][:release_version]}"
+install_dir = "#{node[:storm][:install_root]}/#{storm_name}"
+
 runit_service "nimbus" do
   template_name "storm"
   options :daemon => "nimbus"
