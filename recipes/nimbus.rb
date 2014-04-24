7b4fda76f3fa9934bfc9d8e849fec7b044332802
Homer Strong <homer.strong@gmail.com>
2014-04-24 14:44:13 -0700

    fix artifact naming in each recipe

--- recipes/nimbus.rb
+++ recipes/nimbus.rb
@@ -1,4 +1,9 @@
-storm_name = "storm-#{node[:storm][:release_version]}"
+if node[:storm][:release_name]
+  storm_name = node[:storm][:release_name]
+else
+  storm_name = "storm-#{node[:storm][:release_version]}"
+end
+
 install_dir = "#{node[:storm][:install_root]}/#{storm_name}"
 
 runit_service "nimbus" do
