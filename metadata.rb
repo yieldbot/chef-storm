006023165897b56dfe042ffe94c5227cafaedd2c
Soren Macbeth <soren@dopeness.org>
2014-08-11 10:31:28 -0700

    max_reties not a string

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "dwhite@yieldbot.com"
 license          "All rights reserved"
 description      "Installs/Configures storm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.3"
+version          "0.0.4"
 
 %w{ ntp git java python zeromq silverware }.each do |cb|
   depends cb
